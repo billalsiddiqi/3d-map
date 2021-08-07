56e1e868cf8a701e10e8d8df061a5765fce87b14
Billal Siddiqi <b.sediqi@netlinks.af>
2021-08-07 14:18:04 +0430

    init

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,26 @@
 </template>
 
 <script>
-
+// Import mapbox
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { onMounted } from "vue";
+
+
 export default {
+
   setup() {
     onMounted(() => {
+      // You can get you access token from https://account.mapbox.com/
       mapboxgl.accessToken =
-        "pk.eyJ1IjoiY2FwaTIwNzciLCJhIjoiY2tzMWRvYW5sMXQzZDJwczd4bHk0OGptdSJ9.3tahM0wZbCq3nYrnnxPL8A";
+        "ACCESS TOKEN";
       const map = new mapboxgl.Map({
         container: "map",
         center: [69.17535264597393,34.51983332233756],
         zoom: 18,
-        style: "mapbox://styles/capi2077/cks1kfmir1mrg18o47sstus7e",
+        // You can create a new style in mapbox studio and enable the 3d building and than insert the you style link here
+        style: "YOUR CUSTOMIZED STYLE FROM https://studio.mapbox.com/",
+        // Pitch means the angel of map view which you need in 3d building mode
         pitch: 60,
       });
       map.on('load', () => {
